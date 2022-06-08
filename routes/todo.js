d808e46a1f8f3a5360f839b782a545b064b3e37f
Ezequiel Coronel <nikocor99@gmail.com>
2022-06-08 10:01:46 -0300

    El to-do recibe el item de prueba sin errores

--- routes/todo.js
+++ routes/todo.js
@@ -1,44 +1,32 @@
 var express = require("express");
 var router = express.Router();
 let mysql = require("mysql2");
-let modeloTarea = require("../models").Tarea;
-let actualizar = require("../public/javascripts/actualizarLista");
-
-let refreshList = actualizar.actualizarLista;
+const modelItem = require('../models').Item;
 
 /* GET todo page. */
 router.get("/", async function (req, res, next) {
-  const tareas = await modeloTarea.findAll();
-  res.render("todo", { listaTareas: tareas });
+  const items = await modelItem.findAll();
+  res.render("todo", { listaItems: items });
 });
 
 router.post("/add", async function (req, res, next) {
   let tarea = req.body.nuevaTarea;
   //Insert nueva tarea
-  await modeloTarea
-    .create({ tareaDesc: tarea })
-    .then((data) => {
-      console.log("Se añadio la tarea", data);
-    })
-    .catch((error) => {
-      console.error("Ocurrio un error en el añadido", error);
-    });
-  const tareas = await modeloTarea.findAll();
-  res.render("todo", { listaTareas: tareas });
+  
 });
 
-router.post("/delete/:tareaid", async function (req, res) {
-  modeloTarea
-    .destroy({ where: { idTarea: req.params.tareaid } })
-    .then((data) => {
-      console.log("Se elimino la tarea", data);
-    })
-    .catch((error) => {
-      console.error("No se pudo eliminar la tarea", error);
-    });
+// router.post("/delete/:tareaid", async function (req, res) {
+//   // modeloTarea
+//   //   .destroy({ where: { idTarea: req.params.tareaid } })
+//   //   .then((data) => {
+//   //     console.log("Se elimino la tarea", data);
+//   //   })
+//   //   .catch((error) => {
+//   //     console.error("No se pudo eliminar la tarea", error);
+//   //   });
 
-  const tareas = await modeloTarea.findAll();
-  res.render("todo", { listaTareas: tareas });
-});
+//   // const tareas = await modeloTarea.findAll();
+//   // res.render("todo", { listaTareas: tareas });
+// });
 
 module.exports = router;
