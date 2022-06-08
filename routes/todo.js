0cedb0b389960212910a7f469835abf70760c619
Ezequiel Coronel <nikocor99@gmail.com>
2022-06-08 10:20:33 -0300

    los post y get de todo funcionan correctamente

--- routes/todo.js
+++ routes/todo.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 let mysql = require("mysql2");
-const modelItem = require('../models').Item;
+const modelItem = require("../models").Item;
 
 /* GET todo page. */
 router.get("/", async function (req, res, next) {
@@ -10,23 +10,37 @@ router.get("/", async function (req, res, next) {
 });
 
 router.post("/add", async function (req, res, next) {
-  let tarea = req.body.nuevaTarea;
+  let descripcionItem = req.body.nuevaTarea;
   //Insert nueva tarea
-  
+  const nuevoItem = await modelItem
+    .create({
+      fecha_resolucion: null,
+      descripcion: descripcionItem,
+      prioridad: "baja",
+    })
+    .then((data) => {
+      console.log("El item fue cargado", data);
+    })
+    .catch((error) => {
+      console.log("No se pudo cargar el item", error);
+    });
+
+  const items = await modelItem.findAll();
+  res.render("todo", { listaItems: items });
 });
 
-// router.post("/delete/:tareaid", async function (req, res) {
-//   // modeloTarea
-//   //   .destroy({ where: { idTarea: req.params.tareaid } })
-//   //   .then((data) => {
-//   //     console.log("Se elimino la tarea", data);
-//   //   })
-//   //   .catch((error) => {
-//   //     console.error("No se pudo eliminar la tarea", error);
-//   //   });
+router.post("/delete/:tareaid", async function (req, res) {
+  modelItem
+    .destroy({ where: { idItem: req.params.tareaid } })
+    .then((data) => {
+      console.log("Se elimino el item con idItem: ", data);
+    })
+    .catch((error) => {
+      console.error("No se pudo eliminar el item", error);
+    });
 
-//   // const tareas = await modeloTarea.findAll();
-//   // res.render("todo", { listaTareas: tareas });
-// });
+  const items = await modelItem.findAll();
+  res.render("todo", { listaItems: items });
+});
 
 module.exports = router;
