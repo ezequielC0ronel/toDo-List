e23a31e3741fd90cba5d77e02dd09a342edb9611
Ezequiel Coronel <nikocor99@gmail.com>
2022-06-09 19:30:51 -0300

    Index listo para devolver las listas creadas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+let modelLista= require('../models').Lista;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {listaTareas: []});
+router.get('/', async function(req, res, next) {
+  const listas = await modelLista.findAll();
+  res.render('index', {listaTareas: listas});
 });
 
 module.exports = router;
