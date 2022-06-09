376ffa23eb89fb5832c79bddffd432f4f921d8b7
Ezequiel Coronel <nikocor99@gmail.com>
2022-06-08 22:25:51 -0300

    Agregado el router crear. Podriamos crear listas e items

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 let todoRouter = require('./routes/todo');
+let crearRouter = require('./routes/crear');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/todo', todoRouter);
+app.use('/crear', crearRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
